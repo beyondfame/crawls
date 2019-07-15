84f9b609f49fff8810ce93ec39e8b7764ac3cffe
chonseyt <chonseyt@gmail.com>
2019-07-15 09:22:34 -0500

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+# Collect and parse first page
+page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
+soup = BeautifulSoup(page.text, 'html.parser')
+
+# Pull all text from the BodyText div
+artist_name_list = soup.find(class_='BodyText')
+
+# Pull text from all instances of <a> tag within BodyText div
+artist_name_list_items = artist_name_list.find_all('a')
+
+...
+artist_name_list = soup.find(class_='BodyText')
+artist_name_list_items = artist_name_list.find_all('a')
+
+# Create for loop to print out all artists' names
+for artist_name in artist_name_list_items:
+    print(artist_name.prettify())
+
+# Create a file to write to, add headers row
+f = csv.writer(open('z-artist-names.csv', 'w'))
+f.writerow(['Name', 'Link'])
+
+artist_name_list = soup.find(class_='BodyText')
+artist_name_list_items = artist_name_list.find_all('a')
+
+for artist_name in artist_name_list_items:
+    names = artist_name.contents[0]
+    links = 'https://web.archive.org' + artist_name.get('href')
+
+
+    # Add each artist’s name and associated link to a row
+    f.writerow([names, links])
